f2ddbc19a7b762ece8a409ddf08ba6b32ee9fc4d
Andrew Drobozin <crash1601@gmail.com>
2018-03-31 13:17:17 +0300

    03_31

--- src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
+++ src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
@@ -18,17 +18,152 @@
 package ru.sbt.jschool.session2;
 
 import java.io.PrintStream;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  */
 public class OutputFormatter {
     private PrintStream out;
+    private int[] columnsLen;
+    private String topBorder;
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
-    public OutputFormatter(PrintStream out) {
+    private String patternDecimal = "###,###.###";
+    private DecimalFormat decimalFormat;
+    private String patternNumber = "###.###";
+    private DecimalFormat decimalNumber = new DecimalFormat(patternNumber);
+    public OutputFormatter(PrintStream out)
+
+    {
+        NumberFormat nf = NumberFormat.getNumberInstance(new Locale("fr_FR"));
+        decimalFormat = (DecimalFormat)nf;
+        decimalFormat.applyPattern(patternDecimal);
         this.out = out;
     }
 
+    StringBuilder res = new StringBuilder();
+
     public void output(String[] names, Object[][] data) {
-        //TODO: implement me.
+        initColumnsLen(names, data);
+        topBorder = getTopBorder();
+        int i=0;
+        drawTable(names, data, i);
+        out.print(topBorder);
+        res.append(topBorder);
+    }
+
+    public void drawTable(String[] names, Object[][] data, int i){
+        res.append(topBorder);
+        out.print(topBorder);
+        String str = getСontent(names, data, i);
+        out.print(str);
+        res.append(str);
+
+        if(i >= data.length)
+            return;
+        else
+            drawTable(names, data, ++i);
+
+    }
+
+    public String getСontent(String[] names, Object[][] data, int i){
+        StringBuilder resBuild = new StringBuilder();
+        if(i==0){
+            for(int j=0; j<names.length; j++){
+                StringBuilder curBuild = new StringBuilder();
+                curBuild.append("|");
+                if(names[j].length() == columnsLen[j]){
+                    curBuild.append(names[j]);
+                }else{
+                    int leftSpaces = (int)(columnsLen[j] - names[j].length()) / 2;
+                    while(leftSpaces-- > 0){
+                        curBuild.append(" ");
+                    }
+                    curBuild.append(names[j]);
+                    while(curBuild.length() <= columnsLen[j]){
+                        curBuild.append(" ");
+                    }
+                }
+                resBuild.append(curBuild);
+            }
+            resBuild.append("|\n");
+        }else{
+
+            for(int j=0; j<data[i-1].length; j++){
+                StringBuilder curBuild = new StringBuilder();
+                resBuild.append("|");
+                if(data[i-1][j] instanceof String){
+                    curBuild.append(data[i-1][j].toString());
+                    while(curBuild.length() < columnsLen[j]){
+                        curBuild.append(" ");
+                    }
+                }else{
+                    String str;
+                    if(data[i-1][j] instanceof Date){
+                        str = sdf.format(data[i-1][j]);
+                    }else{
+                        if(data[i-1][j] instanceof Double || data[i-1][j] instanceof Float){
+
+
+                            str = decimalFormat.format(Double.valueOf(data[i-1][j].toString()));
+
+                        }else{
+                            str = data[i-1][j].toString();
+                        }
+                        //str = data[i-1][j].toString().replace('.', '.');
+                    }
+                    int count = 0;
+                    while(columnsLen[j]-str.length() > count++){
+                        curBuild.append(" ");
+                    }
+                    curBuild.append(str);
+                }
+                resBuild.append(curBuild);
+            }
+            resBuild.append("|\n");
+        }
+
+        return resBuild.toString();
+    }
+
+    public void initColumnsLen(String[] names, Object[][] data){
+        columnsLen = new int[names.length];
+        for(int i=0; i<data[0].length; i++){
+            int maxLen = 0;
+            for (int j=0; j<data.length; j++){
+                int curLen = 0;
+                if(data[j][i] instanceof Date){
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+                    curLen = sdf.format(data[j][i]).length();
+                }else {
+                    curLen = data[j][i].toString().length();
+                }
+
+                if(curLen > maxLen){
+                    maxLen = curLen;
+                }
+            }
+            columnsLen[i] = maxLen;
+        }
+    }
+
+    public String getTopBorder(){
+        StringBuilder resBuild = new StringBuilder();
+
+        for(int i=0; i<columnsLen.length; i++){
+            resBuild.append("+");
+            StringBuilder curBuild = new StringBuilder();
+            while(curBuild.length() != columnsLen[i]){
+                curBuild.append("-");
+            }
+            resBuild.append(curBuild.toString());
+        }
+        resBuild.append("+\n");
+        return  resBuild.toString();
     }
 }
+
